536e3c52a4265fc61a2bb54bbd66672b6bf3fbee
breduarm <bryan.e.armijos@gmail.com>
2024-07-14 20:00:01 -0500

    refac(format): Rearrange imports and add some formating on useFetchMovies

--- src/hooks/useFetchMovies.ts
+++ src/hooks/useFetchMovies.ts
@@ -1,30 +1,29 @@
-import { useEffect, useState } from "react"
-import Movie from "../types/entities/Movie";
-import { getMovies } from "../services/MovieService";
-import { MovieResponse } from "../types/responses/MovieResponse";
+import { useEffect, useState } from 'react';
+import { getMovies } from '../services/MovieService';
+import Movie from '../types/entities/Movie';
 
 const useFetchMovies = () => {
-    const [loading, setLoading] = useState(true);
-    const [data, setData] = useState<Movie[]>([]);
-    const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState<Movie[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        fetchMovies();
-    }, [])
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
-    const fetchMovies = async () => {
-        try {
-            const movies = await getMovies();
-            setData(movies);
-        } catch (error) {
-            console.log(error);
-            setError("There was an error trying to fetch movies");
-        } finally {
-            setLoading(false);
-        }
+  const fetchMovies = async () => {
+    try {
+      const movies = await getMovies();
+      setData(movies);
+    } catch (error) {
+      console.log(error);
+      setError('There was an error trying to fetch movies');
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return { data, loading, error };
-}
+  return {data, loading, error};
+};
 
-export default useFetchMovies
+export default useFetchMovies;
